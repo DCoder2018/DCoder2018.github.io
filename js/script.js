e01e5f7a528a75729f69f6e244ab6976be68c8a5
Ismail Minjasar <44488026+DCoder2018@users.noreply.github.com>
2018-10-26 23:27:07 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,11 +13,18 @@ $('document').ready(function(){
 		}
 	});
 	$('.m').click( function(){
-	        var scroll_el = $(this).attr('href');
-	        var destination = $(scroll_el).offset().top;
-	        if ($(scroll_el).length != 0) {
-	            $('html, body').animate( { scrollTop: destination }, 1500 );
-	        }
-	        return false;
-	    });
+	    var scroll_el = $(this).attr('href');
+	    var destination = $(scroll_el).offset().top;
+	    if ($(scroll_el).length != 0) {
+	        $('html, body').animate( { scrollTop: destination }, 400 );
+	    }
+	    return false;
+	});
+    $('.toggle').on("click", function() {
+        $('nav').slideToggle(400, function() {
+            if($(this).css('display') == 'none'){
+        	    $(this).removeAttr('style');
+            }
+        });
+    });
 });
